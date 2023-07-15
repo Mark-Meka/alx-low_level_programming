32329c7115e521146ff2d31bf504914153544bdc
Mark-Meka <marcomeka2003@gmail.com>
2023-07-15 10:29:46 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
 * main - Entry point
@@ -6,8 +6,7 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)
-{
+int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0);
 }
