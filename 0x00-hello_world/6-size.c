c3bd118ea0c7215e4c096e93244a84d28c4ce44e
Mark-Meka <marcomeka2003@gmail.com>
2023-07-15 10:47:19 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
  #include <stdio.h>
- /*
-         Size of a char: 1 byte(s)
-         Size of an int: 4 byte(s)
-         Size of a long int: 4 byte(s)
-         Size of a long long int: 8 byte(s)
-	 Size of a float: 4 byte(s)
- */ 
- int main()
+ /**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+ int main(void)
 {       
          printf("Size of a char: %zu byte(s)",sizeof(char));
 	 printf("Size of an int: %zu byte(s)",sizeof(int));
