795deb8c30af8413a7bd0ec2cdcc3213468bd26e
Mark-Meka <marcomeka2003@gmail.com>
2023-07-16 05:14:25 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
+	
+	for (i = 0; i < 26; i++)
+		putchar(alp[i]);
+	putchar('\n')
+	return (0);	
+}
