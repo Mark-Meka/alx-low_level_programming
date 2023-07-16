50c5522fd84e356923c6279a679d0f9bf6f31882
Mark-Meka <marcomeka2003@gmail.com>
2023-07-16 04:25:49 -0700

    alphabts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,8 @@ int main(void)
 	for (i = 0; i < 26; i++)
 	{
 		if (alp[i] == 'e' || alp[i] == 'q')
-			i+=1;
-	        putchar(alp[i]);
+			i += 1;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
