efc74a913a34fd65c1568850d73bc99fe9551a30
Mark-Meka <marcomeka2003@gmail.com>
2023-07-18 11:38:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - printting _putchar
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void){
+	int i;
+	for (i = 0; i < 1; i++)
+	{
+		printf("_putchar");
+	}
+	return (0);
+}
