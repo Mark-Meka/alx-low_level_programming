ad599487ca99f5b7224cd564df8cb5ab49349c8e
Mark-Meka <marcomeka2003@gmail.com>
2023-07-20 14:31:00 -0700

    loop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,11 @@ void print_most_numbers(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-		_putchar(number);
-
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
 	_putchar('\n');
 }
