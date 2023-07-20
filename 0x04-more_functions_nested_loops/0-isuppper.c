d7fc62e3eabda15ff140342f34d17cece65a066e
Mark-Meka <marcomeka2003@gmail.com>
2023-07-20 08:11:10 -0700

    loop

---
+++ 0x04-more_functions_nested_loops/0-isuppper.c
@@ -0,0 +1,24 @@
+  1 #include "main.h"
+  2 #include <stdio.h>
+  3 
+  4 /**
+  5  * main - check the code.
+  6  *
+  7  * Return: Always 0.
+  8  */
+  9 int _isupper(int c)
+ 10 {
+ 11     char c;
+ 12 
+ 13     if (c >= 'A' && c <= 'Z')
+ 14     {
+ 15             return (1);
+ 16     }
+ 17 
+ 18     else
+ 19     {
+ 20         return (0);
+ 21     }
+ 22 }
+~                                                                                                                                                                                         
+~        
