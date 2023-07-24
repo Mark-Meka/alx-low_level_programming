59cedfcc2ba1ea04a412bb98a6c87b59eb86263d
Mark-Meka <marcomeka2003@gmail.com>
2023-07-25 02:27:18 +0300

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,14 +6,8 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void reset_to_98(int *n)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
     *n = 98;
-    printf("n=%d\n", n);
     return (0);
 }
