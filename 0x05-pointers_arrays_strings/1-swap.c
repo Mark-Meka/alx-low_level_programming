490dd6811a9299678e5f6e7b56cce346d22fd235
Mark-Meka <marcomeka2003@gmail.com>
2023-07-25 00:16:27 -0700

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,15 +8,7 @@
  */
 void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    *a = b;
-    *b = a;
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+    *a = 42;
+    *b = 98;
 }
