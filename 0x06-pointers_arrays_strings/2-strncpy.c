7f785ed38edae3bdf7fb8208cb0fd0bdeb2a4345
Mark-Meka <marcomeka2003@gmail.com>
2023-07-27 15:54:04 +0300

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,12 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int count;
 
 	for (count = 0; count < n && src[count] != '\0'; count++)
-	{
 		dest[count] = src[count];
-	}
-
 	for (; count < n; count++)
 		dest[count] = '\0';
-
 	return (dest);
 }
