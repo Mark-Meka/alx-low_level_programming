c1c7e2478e88c6e0c4a2ea6806a2b9a90316c42b
Mark-Meka <marcomeka2003@gmail.com>
2023-07-27 23:57:53 +0300

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
  *value greater than 0 if string is greater than the other.
  *0 if strings are equal.
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i=0, Value;
+	int counter, cmpVal;
 
-	while ( s1[i] == s2[i]; && s1[i] != '\0')
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		i++;
+		counter++;
 	}
-	
-	Value = s1[i] - s2[i];
-	return (Value);
+
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
 }
