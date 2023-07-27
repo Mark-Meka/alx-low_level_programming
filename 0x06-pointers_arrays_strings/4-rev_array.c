205e831eea09f0c524dae8536d91a90c4dd45f3f
Mark-Meka <marcomeka2003@gmail.com>
2023-07-28 00:01:50 +0300

    pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-   *reverse_array - revereses an array.
-    *@a: pointer to array.
-     *@n: number of elements of an array.
-      *
-       *Return: void.
-        */
+ *reverse_array - revereses an array.
+ *@a: pointer to array.
+ *@n: number of elements of an array.
+ *
+ *Return: void.
+ */
 void reverse_array(int *a, int n)
 {
 	int temp, counter;
