195903a81254339eae5c4fd8dcfa802fa9e903c3
Mark-Meka <marcomeka2003@gmail.com>
2023-07-27 15:52:02 +0300

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,16 @@
   *
   * Return: pointer to destination string.
   */
+
 char *_strncpy(char *dest, char *src, int n)
 { 
 	int count;
-	
+
 	for (count = 0; count < n && src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
 	}
-	
+
 	for (; count < n; count++)
 		dest[count] = '\0';
 
