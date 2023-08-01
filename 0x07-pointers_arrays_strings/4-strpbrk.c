5ac92e1ee86286ce6bc488f6a1335600a82ee5e7
Mark-Meka <marcomeka2003@gmail.com>
2023-08-02 00:19:37 +0300

    pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -17,7 +17,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0;accept[j] != s[i]; j++)
+		for (j = 0;accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
