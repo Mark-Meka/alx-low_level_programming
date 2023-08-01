b56f935968ab67f326e501d73872db7e7b89d4b2
Mark-Meka <marcomeka2003@gmail.com>
2023-08-01 15:29:16 +0300

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
   *_strspn - gets the lenght of a prefix substring.
   *@s: pointer to strin.
@@ -10,10 +11,11 @@
   *segment of s which consist only of bytes from
   *accept.
   */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
