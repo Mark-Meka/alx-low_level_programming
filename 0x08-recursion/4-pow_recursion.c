79c9567d367133307ea6b1dcdf011189425827b2
Mark-Meka <marcomeka2003@gmail.com>
2023-08-04 00:52:13 +0300

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,9 +12,10 @@ int _pow_recursion(int x, int y)
 	
 	if (y < 0)
 		return (-1);
-	else
+	else if (y != 0)
 	{
-		n = _pow_recursion(x, y);
+		return (x * _pow_recursion(x, y));
 	}
-	return (n);
+	else
+		return (1);
 }
