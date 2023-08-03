152fec6d10dd21db3f573de70e2d7a824cc27bbb
Mark-Meka <marcomeka2003@gmail.com>
2023-08-03 22:12:46 +0300

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  *_puts_recursion - prints a string followed by a new line.
+  *@s: pointer to string.
+  *
+  *Return: void.
+  */
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+		return (*s);
+	return (0);
+}
