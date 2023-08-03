4487a32c2da52258ddfa89b59003784d40d658a6
Mark-Meka <marcomeka2003@gmail.com>
2023-08-03 22:53:36 +0300

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_strlen_recursion(s + 1)
+			_putchar(*s);
+	}
+}
