8230f7669e62b9e351c3f995641ec17c5627ba06
Mark-Meka <marcomeka2003@gmail.com>
2023-08-05 01:11:57 +0300

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
   */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-
-	printf("%s\n", argv[0];
-	printf("%s\n", argv[1];
-	printf("%s\n", argv[2];
+	prinf("%d\n", argc);
+	printf("%s\n", argv[0]);
+	printf("%s\n", argv[1]);
+	printf("%s\n", argv[2]);
 	return (0);
 }
