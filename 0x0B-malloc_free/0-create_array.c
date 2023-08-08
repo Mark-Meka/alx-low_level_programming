35613f5a4bb42d8d1a8b9746f07eabfd6bf36cde
Mark-Meka <marcomeka2003@gmail.com>
2023-08-08 15:43:45 +0300

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,21 +7,17 @@
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: The pointer to the arrays or NULL.
  */
 char *create_array(unsigned int size, char c)
 {
-	int arr[size];
-	int i;
+	char *p = malloc(size);
 
-	if (size == 0)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		arr[i];
-	}
-	return (0);
+	if (size == 0 || p == 0)
+		return (0);
+	while (size--)
+		n[size] = c;
+	return (n);
 }
 
 
