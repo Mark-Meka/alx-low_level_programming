2cd8c32a918e5dc6fe7599f33b0dab37f1742c7a
Mark-Meka <marcomeka2003@gmail.com>
2023-08-15 00:21:40 +0300

    dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 
 	if ((*d).owner == NULL)
 		(*d).owner = "(nil)";
-	printf("Name: %s", (*d).name);
-	printf("Age: %f", (*d).age);
-	printf("Owner: %s", (*d).owner);
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
 }
