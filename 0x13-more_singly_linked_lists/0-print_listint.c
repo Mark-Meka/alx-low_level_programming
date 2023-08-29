7ff40e6c71dbe3931b9dbf5944e7ac096e59a784
Mark-Meka <marcomeka2003@gmail.com>
2023-08-29 18:43:09 +0300

    linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a list.
+ * print_listint - Prints all the elements of a list.
  * @h: head of a list.
  *
- * Return: numbers of nodes.
+ * Return: Numbers of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
